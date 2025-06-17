90154224e525a3f13aeee3647a0143cccddd0929
hibinonature <206782190+hibino847@users.noreply.github.com>
2025-06-17 20:59:52 +0900

    udemy 102-104

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -29,7 +29,7 @@ const List = (): JSX.Element => {
         const unsubscribe = onSnapshot(q, (snapshot) => {
             const remoteMemos: Memo[] = []
             snapshot.forEach((doc) => {
-                console.log('memo', doc.data())
+                //console.log('memo', doc.data())
                 const { bodyText, updatedAt } = doc.data()
                 remoteMemos.push({
                     id: doc.id,
@@ -49,7 +49,7 @@ const List = (): JSX.Element => {
             />
             <CircleButton onPress={handlePress}>
                 <Icon name='plus' size={40} color='#ffffff' />
-            </CircleButton>WE
+            </CircleButton>
         </View>
     )
 }
